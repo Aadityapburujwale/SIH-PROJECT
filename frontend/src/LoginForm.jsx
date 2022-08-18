573a5aa948735788e5cceee39c3db026c49cc1d6
Aadityapburujwale <aadityapburujwale@gmail.com>
2022-08-18 09:48:13 +0530

    Admin Login Form Added.

--- frontend/src/LoginForm.jsx
+++ frontend/src/LoginForm.jsx
@@ -4,6 +4,45 @@ export default function LoginForm({ setIsAdminLoggedIn }) {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
 
+
+  const formStyle = {
+    margin: "200px auto auto auto",
+    padding: "10px",
+    border: "1px solid #c9c9c9",
+    borderRadius: "5px",
+    background: "#f5f5f5",
+    width: "220px",
+    display: "block"
+  };
+  
+  const labelStyle = {
+    margin: "10px 0 5px 0",
+    fontFamily: "Arial, Helvetica, sans-serif",
+    fontSize: "15px"
+  };
+  
+  const inputStyle = {
+    margin: "5px 0 10px 0",
+    padding: "5px",
+    border: "1px solid #bfbfbf",
+    borderRadius: "3px",
+    boxSizing: "border-box",
+    width: "100%"
+  };
+  
+  const submitStyle = {
+    margin: "10px 0 0 0",
+    padding: "7px 10px",
+    border: "1px solid #efffff",
+    borderRadius: "3px",
+    background: "#3085d6",
+    width: "100%",
+    fontSize: "15px",
+    color: "white",
+    display: "block"
+  };
+  
+
   async function handleSubmit(e) {
     e.preventDefault();
 
@@ -25,6 +64,7 @@ export default function LoginForm({ setIsAdminLoggedIn }) {
         if (response.success) {
           window.localStorage.setItem("isAdminLoggedIn", "true");
           setIsAdminLoggedIn(true);
+
         } else {
           alert("wrong username and password");
           setUserName("");
@@ -36,9 +76,10 @@ export default function LoginForm({ setIsAdminLoggedIn }) {
 
   return (
     <>
-      <form onSubmit={handleSubmit} style={{ margin: "50px" }}>
-        <label htmlFor="username"></label>
+      <form onSubmit={handleSubmit} style={formStyle}>
+        <label htmlFor="username" style={labelStyle}>Username</label>
         <input
+        style={inputStyle}
           type="text"
           name="username"
           id="username"
@@ -47,8 +88,9 @@ export default function LoginForm({ setIsAdminLoggedIn }) {
             setUserName(e.target.value);
           }}
         />
-        <label htmlFor="password"></label>
+        <label htmlFor="password" style={labelStyle}>Password</label>
         <input
+        style={inputStyle}
           type="text"
           name="password"
           id="password"
@@ -57,7 +99,7 @@ export default function LoginForm({ setIsAdminLoggedIn }) {
             setPassword(e.target.value);
           }}
         />
-        <button type="submit">submit form</button>
+        <button type="submit" style={submitStyle}>submit form</button>
       </form>
     </>
   );
