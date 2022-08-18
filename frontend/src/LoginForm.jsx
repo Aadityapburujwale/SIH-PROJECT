ceb54d36c315627e92b4630f204d62ae65f5a8e5
Aadityapburujwale <aadityapburujwale@gmail.com>
2022-08-18 16:07:09 +0530

    Conflicts Removed

--- frontend/src/LoginForm.jsx
+++ frontend/src/LoginForm.jsx
@@ -5,7 +5,6 @@ const appStyle = {
   display: "flex",
 };
 
-<<<<<<< HEAD
 const formStyle = {
   margin: "auto",
   padding: "10px",
@@ -15,49 +14,6 @@ const formStyle = {
   width: "220px",
   display: "block",
 };
-=======
-
-  const formStyle = {
-    margin: "200px auto auto auto",
-    padding: "10px",
-    border: "1px solid #c9c9c9",
-    borderRadius: "5px",
-    background: "#f5f5f5",
-    width: "220px",
-    display: "block"
-  };
-  
-  const labelStyle = {
-    margin: "10px 0 5px 0",
-    fontFamily: "Arial, Helvetica, sans-serif",
-    fontSize: "15px"
-  };
-  
-  const inputStyle = {
-    margin: "5px 0 10px 0",
-    padding: "5px",
-    border: "1px solid #bfbfbf",
-    borderRadius: "3px",
-    boxSizing: "border-box",
-    width: "100%"
-  };
-  
-  const submitStyle = {
-    margin: "10px 0 0 0",
-    padding: "7px 10px",
-    border: "1px solid #efffff",
-    borderRadius: "3px",
-    background: "#3085d6",
-    width: "100%",
-    fontSize: "15px",
-    color: "white",
-    display: "block"
-  };
-  
-
-  async function handleSubmit(e) {
-    e.preventDefault();
->>>>>>> 15ca2062c9203397b5686d24fd2d61cbd0b81042
 
 const labelStyle = {
   margin: "10px 0 5px 0",
@@ -65,7 +21,6 @@ const labelStyle = {
   fontSize: "15px",
 };
 
-<<<<<<< HEAD
 const inputStyle = {
   margin: "5px 0 10px 0",
   padding: "5px",
@@ -74,30 +29,6 @@ const inputStyle = {
   boxSizing: "border-box",
   width: "100%",
 };
-=======
-    fetch("http://localhost:2000", {
-      method: "POST",
-      headers: {
-        "Content-type": "Application/json",
-      },
-      body: JSON.stringify(loginDetails),
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        // if response is true from the backend
-        if (response.success) {
-          window.localStorage.setItem("isAdminLoggedIn", "true");
-          setIsAdminLoggedIn(true);
-
-        } else {
-          alert("wrong username and password");
-          setUserName("");
-          setPassword("");
-          setIsAdminLoggedIn(false);
-        }
-      });
-  }
->>>>>>> 15ca2062c9203397b5686d24fd2d61cbd0b81042
 
 const submitStyle = {
   margin: "10px 0 0 0",
@@ -113,7 +44,6 @@ const submitStyle = {
 
 const Field = React.forwardRef(({ label, type }, ref) => {
   return (
-<<<<<<< HEAD
     <div>
       <label style={labelStyle}>{label}</label>
       <input ref={ref} type={type} style={inputStyle} />
@@ -161,34 +91,3 @@ const LoginForm = () => {
 };
 
 export default LoginForm;
-=======
-    <>
-      <form onSubmit={handleSubmit} style={formStyle}>
-        <label htmlFor="username" style={labelStyle}>Username</label>
-        <input
-        style={inputStyle}
-          type="text"
-          name="username"
-          id="username"
-          value={username}
-          onChange={(e) => {
-            setUserName(e.target.value);
-          }}
-        />
-        <label htmlFor="password" style={labelStyle}>Password</label>
-        <input
-        style={inputStyle}
-          type="text"
-          name="password"
-          id="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <button type="submit" style={submitStyle}>submit form</button>
-      </form>
-    </>
-  );
-}
->>>>>>> 15ca2062c9203397b5686d24fd2d61cbd0b81042
