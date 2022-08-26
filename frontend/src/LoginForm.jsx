12ae41b9e5f246cef42951725dacafedcd9e5c41
Harshad Marathe <harshadmarathe8@gmail.com>
2022-08-26 16:40:46 +0530

    Login Form Bug Fixed

--- frontend/src/LoginForm.jsx
+++ frontend/src/LoginForm.jsx
@@ -73,78 +73,82 @@ export default function LoginForm({ setIsAdminLoggedIn }) {
 
   return (
     <>
-      <div className="limiter">
-        <div className="container-login100">
-          <div className="wrap-login100">
-            <div className="login100-pic js-tilt" data-tilt>
-              <img
-                src={process.env.PUBLIC_URL + "/sji.png"}
-                alt="IMG"
-                style={{ marginLeft: "11%", width: "220px" }}
-              />
+      <body style={{ marginTop: "2vh" }}>
+        <div className="limiter">
+          <div className="container-login100">
+            <div className="wrap-login100">
+              <div className="login100-pic js-tilt" data-tilt>
+                <img
+                  src={process.env.PUBLIC_URL + "/sji.png"}
+                  alt="IMG"
+                  style={{ marginLeft: "11%", width: "220px" }}
+                />
 
-              {/* <img
+                {/* <img
                 src="https://colorlib.com/etc/lf/Login_v1/images/img-01.png"
                 alt="IMG"
               /> */}
-            </div>
-            <form
-              onSubmit={handleSubmit}
-              className="login100-form validate-form"
-            >
-              <span className="login100-form-title">Police Officer Login</span>
-              <div
-                className="wrap-input100 validate-input"
-                data-validate="Valid email is required: ex@abc.xyz"
+              </div>
+              <form
+                onSubmit={handleSubmit}
+                className="login100-form validate-form"
               >
-                <input
-                  className="input100"
-                  type="text"
-                  name="Username"
-                  placeholder="Username"
-                  onChange={(e) => {
-                    setUserName(e.target.value);
-                  }}
-                  value={username}
-                />
-                <span className="focus-input100"></span>
-                <span className="symbol-input100">
-                  <i className="fa fa-user" aria-hidden="true"></i>
+                <span className="login100-form-title">
+                  Police Officer Login
                 </span>
-              </div>
-              <div
-                className="wrap-input100 validate-input"
-                data-validate="Password is required"
-              >
-                <input
-                  className="input100"
-                  type="password"
-                  name="pass"
-                  placeholder="Password"
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                  value={password}
-                />
-                <span className="focus-input100"></span>
-                <span className="symbol-input100">
-                  <i className="fa fa-lock" aria-hidden="true"></i>
-                </span>
-              </div>
-              <div className="container-login100-form-btn">
-                <button type="submit" className="login100-form-btn">
-                  Login
-                </button>
-              </div>
+                <div
+                  className="wrap-input100 validate-input"
+                  data-validate="Valid email is required: ex@abc.xyz"
+                >
+                  <input
+                    className="input100"
+                    type="text"
+                    name="Username"
+                    placeholder="Username"
+                    onChange={(e) => {
+                      setUserName(e.target.value);
+                    }}
+                    value={username}
+                  />
+                  <span className="focus-input100"></span>
+                  <span className="symbol-input100">
+                    <i className="fa fa-user" aria-hidden="true"></i>
+                  </span>
+                </div>
+                <div
+                  className="wrap-input100 validate-input"
+                  data-validate="Password is required"
+                >
+                  <input
+                    className="input100"
+                    type="password"
+                    name="pass"
+                    placeholder="Password"
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                    value={password}
+                  />
+                  <span className="focus-input100"></span>
+                  <span className="symbol-input100">
+                    <i className="fa fa-lock" aria-hidden="true"></i>
+                  </span>
+                </div>
+                <div className="container-login100-form-btn">
+                  <button type="submit" className="login100-form-btn">
+                    Login
+                  </button>
+                </div>
 
-              <div className="text-center p-t-136">
-                <button type="submit"></button>
-                <a className="txt2" href="#"></a>
-              </div>
-            </form>
+                <div className="text-center p-t-136">
+                  <button type="submit"></button>
+                  <a className="txt2" href="#"></a>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      </body>
     </>
   );
 }
